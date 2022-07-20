b855289e2a5742b2fc4dba3d3950ac721bd6511a
Igor Lukjanov <lukjanov.igr@gmail.com>
2022-07-20 09:37:21 +0300

    Add header animation

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -1,18 +1,9 @@
 const header = document.querySelector('.header');
 const toggleButton = document.querySelector('.toggle-button');
+const headerLogo = document.querySelector('.header__logo');
+const navbarEl = document.querySelector('.navbar');
 const navbarList = document.querySelector('.navbar__list');
 
 toggleButton.addEventListener('click', () => {
   navbarList.classList.toggle('navbar__list-show');
 });
-
-// window.addEventListener('scroll', () => {
-//   console.log(window.screenY, typeof window.screenY);
-//   if (window.screenY > 20) {
-//     header.classList.add('header-scroll');
-//     toggleButton.classList.add('toggle-button-scroll');
-//   } else {
-//     header.classList.remove('header-scroll');
-//     toggleButton.classList.remove('toggle-button-scroll');
-//   }
-// });
